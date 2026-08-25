e287847a02d6cd7798a10d402fc48676033170c2
Samhinophobia <chaae18isthebest@gmail.com>
2026-08-25 18:01:59 -0400

    Updated ServerScriptService/EmoteSystem/Emotes/Stars.lua

---
+++ ServerScriptService/EmoteSystem/Emotes/Stars.lua
@@ -0,0 +1,12 @@
+-- @ScriptType: ModuleScript
+local emote = {}
+emote.LoopAnim = "rbxassetid://70902727380778"
+emote.Audio = "rbxassetid://104053882398835"
+emote.SyncKey = "Stars"
+
+emote.Duo = true
+emote.FollowAnim = "rbxassetid://96979844370410"
+emote.DuoOffset = CFrame.new(0, 0, 0) * CFrame.Angles(0, 0, 0)
+emote.SpiralIcon = "rbxassetid://88984553928479"
+
+return emote
