635aa9032a6532046ee765cc6b16e82f74f5c6ed
Samhinophobia <chaae18isthebest@gmail.com>
2026-08-25 18:02:14 -0400

    Updated ServerScriptService/EmoteSystem/Emotes/LoopPatrico.lua

---
+++ ServerScriptService/EmoteSystem/Emotes/LoopPatrico.lua
@@ -0,0 +1,8 @@
+-- @ScriptType: ModuleScript
+local emote = {}
+
+emote.LoopAnim = "rbxassetid://110353829246544"
+emote.Audio    = "rbxassetid://123500723493077"
+emote.SyncKey  = "LoopPatrico"
+
+return emote
