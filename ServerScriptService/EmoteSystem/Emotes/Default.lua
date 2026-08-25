30e494198b0f8c61d9758b6da24a77078431066e
Samhinophobia <chaae18isthebest@gmail.com>
2026-08-25 18:02:03 -0400

    Updated ServerScriptService/EmoteSystem/Emotes/Default.lua

---
+++ ServerScriptService/EmoteSystem/Emotes/Default.lua
@@ -0,0 +1,8 @@
+-- @ScriptType: ModuleScript
+local emote = {}
+
+emote.LoopAnim = "rbxassetid://94096426224423"
+emote.Audio    = "rbxassetid://99935531759793"
+emote.SyncKey  = "Default"
+
+return emote
